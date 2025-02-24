df3a9c1e759d686eac97b76fff2d3c3874f08072
Informatica <informatica@alumno.local>
2025-02-24 09:04:27 -0600

    feat: Inicio de sesión de administrador

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { config } from "dotenv"
 import { initServer } from "./configs/server.js"
+import { addAdminDefault } from "./src/auth/auth.controller.js"
 
 config()
 initServer()
+addAdminDefault()
---
+++ src/auth/auth.controller.js
@@ -0,0 +1,54 @@
+import { verify,hash } from "argon2"
+import User from "../user/user.model.js"
+import { generateJWT } from "../helpers/generate-jwt.js";
+
+export const addAdminDefault = async() => {
+    const admin = await User.findOne({ role: "ADMIN_ROLE"})
+    if(!admin) {
+        await User.create({
+            username: "Dev_vyn",
+            email: "devGomez@gmail.com",
+            password: await hash("123456789"),
+            role: "ADMIN_ROLE",
+        })
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, username, password } = req.body
+    try{
+        const user = await User.findOne({
+            $or:[{email: email}, {username: username}]
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message: "Crendenciales inválidas",
+                error:"No existe el usuario o correo ingresado"
+            })
+        }
+
+        const validPassword = await verify(user.password, password)
+
+        if(!validPassword){
+            return res.status(400).json({
+                message: "Crendenciales inválidas",
+                error: "Contraseña incorrecta"
+            })
+        }
+
+        const token = await generateJWT(user.id)
+
+        return res.status(200).json({
+            message: "Login successful",
+            userDetails: {
+                token: token,
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            message: "Login failed, server error",
+            error: err.message
+        })
+    }
+}
